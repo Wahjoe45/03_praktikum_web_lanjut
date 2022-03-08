482295377d3c1baf7e51acec50247c6da42820ca
Wahjoe45 <setiawanaryawahjoe@gmail.com>
2022-03-08 20:34:30 +0700

    Penambahan view dan route

--- 03_praktikum_web_lanjut/routes/web.php
+++ 03_praktikum_web_lanjut/routes/web.php
@@ -14,9 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/main', function () {
     return view('main');
 });
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
